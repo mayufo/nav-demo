d0969cf0b274cc29847fa58dfdc0a9d17f0fd29f
mayufo <mayufo@aliyun.com>
2017-11-10 20:22:13 +0800

    complete

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ var keys = [
 
 var nav = document.getElementById('nav')
 var hash = {
-    'q': 'qq.com'
+    'q': 'qq.com',
+    'j': 'jianshu.com',
+    'z': 'zhihu.com'
 }
 
+var localHash = JSON.parse(localStorage.getItem('hash') || 'null')
+
+hash = localHash ? localHash : hash
 for (var i = 0, len = keys.length; i < len; i++) {
     var div = document.createElement('div')
     nav.appendChild(div)
@@ -18,6 +23,7 @@ for (var i = 0, len = keys.length; i < len; i++) {
         var delBtn = document.createElement('a')
 
         kbd.textContent = keys[i][j]
+        kbd.id = keys[i][j]
         kbd.setAttribute('data', keys[i][j])
 
         editBtn.id = 'edit-' + keys[i][j]
@@ -38,14 +44,21 @@ for (var i = 0, len = keys.length; i < len; i++) {
 
         editBtn.onclick = function (e) {
             var url = prompt('请输入' + e.target.innerText + '位键对应网址')
-            hash[e.target.innerText] = url
+            if (url) {
+                hash[e.target.innerText] = url
+                localStorage.setItem('hash', JSON.stringify(hash))
+            }
+            e.stopPropagation();
         }
         delBtn.onclick = function (e) {
             hash[e.target.innerText] = ''
+            e.stopPropagation();
         }
         kbd.onclick = function (e) {
-            console.log(e.target.attributes[0].value)
-            window.open ('http://' + hash[e.target.attributes[0].value], '_black')
+            console.log(e.target.id)
+            if (hash[e.target.id]) {
+                window.open ('http://' + hash[e.target.id], '_black')
+            }
         }
     }
 }
@@ -54,7 +67,16 @@ for (var i = 0, len = keys.length; i < len; i++) {
 
 
 document.onkeypress = function (e) {
+    var active = document.getElementById(e.key)
+    // active.style.backgroundColor = '#ffffff'
+    // active.style.color ='#27ae60'
     if (hash[e.key]) {
         window.open ('http://' + hash[e.key], '_black')
     }
 }
+
+// document.onkeyup = function (e) {
+//     var active = document.getElementById(e.key)
+//     active.style.backgroundColor = '#27ae60'
+//     active.style.color ='#ffffff'
+// }
